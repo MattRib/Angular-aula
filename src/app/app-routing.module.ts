ffc1885698b39c0f5a3f882be0f4d538b0f56950
MattRib <matheus.rib12@hotmail.com>
2018-04-27 20:59:45 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { ServicosComponent } from "./servicos/servicos.component";
+import { ContatosComponent } from "./contatos/contatos.component";
+//import { HeaderComponent } from "./header/header.component";
 //import { Pagina1Component } from './pagina1/pagina1.component';
 //import { Pagina2Component } from './pagina2/pagina2.component';
 
 const routes: Routes = [
-//{ path: '', redirectTo: '/pagina1', pathMatch: 'full' },
-//{ path: 'pagina1', component: Pagina1Component },
-//{ path: 'pagina2', component: Pagina2Component }
+{ path: '', redirectTo: 'home', pathMatch: 'full' },
+{ path: 'home', component: HomeComponent },
+{ path: 'servicos', component: ServicosComponent },
+{ path: 'contatos', component: ContatosComponent }
+//{ path: 'header', component: HeaderComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { ContatosComponent } from './contatos/contatos.component';
+import { HomeComponent } from "./home/home.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ServicosComponent,
+    ContatosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
